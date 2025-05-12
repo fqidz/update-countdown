a16bb7516490f40764109dea76b81b90ee91f50b
fqidz <faidz.arante@gmail.com>
2025-05-12 19:07:44 +0300

    feat: naive (and very inefficient) real time countdown timer

--- assets/scripts/countdown.js
+++ assets/scripts/countdown.js
@@ -11,27 +11,28 @@ function formatDateTime(_evt) {
 
 function formatCountdown(_evt) {
     const countdown_elem = document.getElementById("countdown");
-
-    const now = new Date(Date.now());
     const date_time_elem = document.getElementById("date-time");
-    const date_time = new Date(Date.parse(date_time_elem.textContent));
 
-    const diff_time = date_time.getTime() - now.getTime();
-    if (diff_time > 0) {
-        const diff_millis = Math.floor((diff_time % 1000));
-        const diff_seconds = Math.floor((diff_time / 1000) % 60);
-        const diff_minutes = Math.floor((diff_time / (1000 * 60)) % 60);
-        const diff_hours = Math.floor((diff_time / (1000 * 60 * 60)) % 24);
-        const diff_days = Math.floor(diff_time / (1000 * 60 * 60 * 24));
+    setInterval(function() {
+        const now = new Date(Date.now());
+        const date_time = new Date(Date.parse(date_time_elem.textContent));
+        const diff_time = date_time.getTime() - now.getTime();
+        if (diff_time > 0) {
+            const diff_millis = Math.floor((diff_time % 1000));
+            const diff_seconds = Math.floor((diff_time / 1000) % 60);
+            const diff_minutes = Math.floor((diff_time / (1000 * 60)) % 60);
+            const diff_hours = Math.floor((diff_time / (1000 * 60 * 60)) % 24);
+            const diff_days = Math.floor(diff_time / (1000 * 60 * 60 * 24));
 
-        countdown_elem.textContent = `\
+            countdown_elem.textContent = `\
 ${diff_days}:\
 ${String(diff_hours).padStart(2, "0")}:\
 ${String(diff_minutes).padStart(2, "0")}:\
 ${String(diff_seconds).padStart(2, "0")}.\
 ${String(diff_millis).padStart(3, "0")}\
-        `;
-    } else {
-        countdown_elem.textContent = "0:00:00:00.000";
-    }
+            `;
+        } else {
+            countdown_elem.textContent = "0:00:00:00.000";
+        }
+    }, 1);
 }
