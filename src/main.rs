73a0e02ec9e8bfc1740ec797453692ead0169c74
fqidz <faidz.arante@gmail.com>
2025-07-09 21:22:09 +0300

    feat: add a limit to how many timestamps are sent to users per interval

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,13 @@
-use std::{fs, ops::Range, path::PathBuf, sync::Arc, time::Duration};
+use std::{
+    fs,
+    ops::Range,
+    path::PathBuf,
+    sync::{
+        Arc,
+        atomic::{AtomicI64, AtomicU8, Ordering},
+    },
+    time::Duration,
+};
 
 use askama::Template;
 use axum::{
@@ -22,6 +31,7 @@ use rand::{
 use tokio::{
     signal,
     sync::{RwLock, broadcast},
+    time::interval,
 };
 use tower_http::{compression::CompressionLayer, services::ServeDir, timeout::TimeoutLayer};
 
@@ -149,13 +159,20 @@ async fn websocket(stream: WebSocket, state: Arc<AppState>) {
     let (mut sender, mut reciever) = stream.split();
 
     let tx = state.tx.clone();
+    let num_recieved_in_interval = Arc::new(AtomicU8::new(0));
+
+    let datetime_read = state.datetimes.read().await;
+    let last_timestamp_recieved = Arc::new(AtomicI64::new(
+        datetime_read.get("battlebit").unwrap().timestamp_millis(),
+    ));
+
+    drop(datetime_read);
 
     let mut recieve_task = tokio::spawn({
         let state_cloned = state.clone();
         let mut rng = SmallRng::from_os_rng();
         let secs_range = Uniform::try_from(SECS_INCREMENT_RANGE).unwrap();
         async move {
-            // Increments datetime no matter the message
             while let Some(Ok(Message::Binary(_msg))) = reciever.next().await {
                 let mut datetime_write = state_cloned.datetimes.write().await;
                 let datetime = datetime_write.get_mut("battlebit").unwrap();
@@ -170,16 +187,54 @@ async fn websocket(stream: WebSocket, state: Arc<AppState>) {
 
     let mut rx = state.tx.subscribe();
 
-    // TODO: Strategically decide when to send messages, depending on the amount of users
-    // and volume of recieved messages.
-    let mut send_task = tokio::spawn(async move {
-        while let Ok(msg) = rx.recv().await {
-            if sender
-                .send(Message::Text(Utf8Bytes::from(msg.to_string())))
-                .await
-                .is_err()
-            {
-                break;
+    let mut send_task = tokio::spawn({
+        let state_cloned = state.clone();
+        let mut interval = interval(Duration::from_millis(500));
+        async move {
+            loop {
+                tokio::select! {
+                    Ok(timestamp_msg) = rx.recv() => {
+                        // Fetch, then increment, then also increment fetch'ed value so that it
+                        // matches the incremented value. Basically `add_fetch()`.
+                        let num_messages = num_recieved_in_interval
+                            .fetch_add(1, Ordering::Relaxed)
+                            .saturating_add(1);
+
+                        last_timestamp_recieved.store(timestamp_msg, Ordering::Relaxed);
+
+                        if num_messages <= 10 {
+                            if sender
+                                .send(Message::Text(Utf8Bytes::from(timestamp_msg.to_string())))
+                                .await
+                                .is_err()
+                            {
+                                // TODO: also update `state.datetimes`
+                                break;
+                            }
+                        }
+                    },
+                    _ = interval.tick() => {
+                        let datetime_read = state_cloned.datetimes.read().await;
+                        let datetime = datetime_read.get("battlebit").unwrap();
+                        let current_timestamp = datetime.timestamp_millis();
+
+                        let last_timestamp = last_timestamp_recieved.load(Ordering::Relaxed);
+
+                        if last_timestamp == current_timestamp {
+                            continue;
+                        }
+
+                        if sender
+                            .send(Message::Text(Utf8Bytes::from(last_timestamp.to_string())))
+                            .await
+                            .is_err()
+                        {
+                            break;
+                        }
+
+                        num_recieved_in_interval.store(0, Ordering::Relaxed)
+                    }
+                }
             }
         }
     });
