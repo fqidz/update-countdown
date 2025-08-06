cbf55a9795cece72caccdce2c75e541aa6ca402d
fqidz <faidz.arante@gmail.com>
2025-08-06 17:00:05 +0300

    feat: add Timeout class to wrap around setTimeout()

--- assets/scripts/countdown.js
+++ assets/scripts/countdown.js
@@ -999,6 +999,75 @@ class DatetimeDisplay {
     }
 }
 
+class Timeout {
+    /** @type {number} */
+    #inner_timeout_id
+    /** @type {Function | null} */
+    #handler
+    /** @type {number} */
+    timeout
+    /** @type {boolean} */
+    finished
+
+    /**
+     * @param {Function | null} handler
+     * @param {number} timeout
+     */
+    constructor(handler, timeout = 1000) {
+        this.set(handler, timeout);
+        this.finished = true;
+    }
+
+    /**
+     * @param {Function | null} handler
+     * @param {number} timeout
+     */
+    set(handler, timeout) {
+        this.#handler = handler;
+        this.timeout = timeout;
+    }
+
+    /** @param {Function | null} handler */
+    setHandler(handler) {
+        this.#handler = handler;
+    }
+
+    /** @param {number} timeout */
+    setTimeout(timeout) {
+        this.timeout = timeout;
+    }
+
+    start() {
+        if (this.#handler === null) {
+            this.finished = true;
+            return;
+        }
+        this.finished = false;
+        this.#inner_timeout_id = setTimeout(() => {
+            if (this.#handler !== null) {
+                this.#handler()
+            }
+            this.finished = true;
+        }, this.timeout);
+    }
+
+    cancel() {
+        if (!this.finished) {
+            clearTimeout(this.#inner_timeout_id);
+            this.finished = true;
+        }
+    }
+
+    restart() {
+        if (this.#handler === null) {
+            this.finished = true;
+            return;
+        }
+        this.cancel();
+        this.start();
+    }
+}
+
 class CustomWebSocket {
     /** @type {WebSocket | null} */
     #websocket;
