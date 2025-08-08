9f8fe24123b70bd053248e56d89f9c5275d7eba7
fqidz <faidz.arante@gmail.com>
2025-08-08 18:31:21 +0300

    feat: add user statistics (click & duration added)

--- assets/scripts/countdown.js
+++ assets/scripts/countdown.js
@@ -1267,82 +1267,334 @@ function timeUnitToString(time_unit) {
     }
 }
 
+const UserStatisticState = Object.freeze({
+    AddedDuration: 0,
+    ClickCount: 1,
+})
+
 /**
- * Calculates the largest time unit that can fit the given minutes.
- *
- * ## Examples
- * 57m    -> 57m
- * 1000m  -> 16.67h
- * 17000m -> 1.69w
- * @param {number} minutes
+ * @typedef {Object} UnitDuration
+ * @property {number} time_unit
+ * @property {number} value
+ **/
+
+/**
+ * @property {number} time_unit
+ * @property {number} value
  * @returns {string}
  **/
-function toShortDuration(minutes) {
-    if (minutes > MINUTES_PER_UNIT[TimeUnits.Year]) {
-        const value = minutes / MINUTES_PER_UNIT[TimeUnits.Year];
-        return `${value.toFixed(2)}${timeUnitToString(TimeUnits.Year)}`;
+function formatUnitDuration(time_unit, value) {
+    if (time_unit === TimeUnits.Hour || time_unit === TimeUnits.Minute) {
+        return `${String(value).padStart(2, '0')}${timeUnitToString(time_unit)}`
     }
-    for (let next_time_unit = 1; next_time_unit <= Object.keys(TimeUnits).length; next_time_unit++) {
-        const previous_minutes_per_unit = MINUTES_PER_UNIT[next_time_unit - 1];
-        if (minutes >=  previous_minutes_per_unit && minutes < MINUTES_PER_UNIT[next_time_unit]) {
-            let value = minutes / previous_minutes_per_unit;
-        return `${value.toFixed(2)}${timeUnitToString(next_time_unit - 1)}`;
+    return `${value}${timeUnitToString(time_unit)}`
+}
+
+/**
+ * @typedef {Object} UserStatisticElems
+ * @property {HTMLButtonElement} container
+ * @property {HTMLSpanElement | null} year
+ * @property {HTMLSpanElement | null} month
+ * @property {HTMLSpanElement | null} week
+ * @property {HTMLSpanElement | null} day
+ * @property {HTMLSpanElement | null} hour
+ * @property {HTMLSpanElement | null} minute
+ * @property {HTMLSpanElement | null} click
+ **/
+
+/**
+ * @param {string} id
+ * @throws {Error}
+ * @returns {never}
+ **/
+function assertElementExists(id) {
+    throw new Error(`No element found with id="${id}"`)
+}
+
+/**
+ * Takes in `{I | null}` and returns `{I}`. Throws an `Error` if it's null.
+ * Kind of like `Option::unwrap()` in rust.
+ *
+ * ## Example
+ *
+ * ```js
+ * // this has type of {HTMLElement | null}
+ * const elem = document.getElementById("foo");
+ *
+ * // this has type of {HTMLElement}
+ * const foo = unwrapSome(elem);
+ * ```
+ * @template {any} I
+ * @param {I | null} item
+ * @throws {Error}
+ * @returns {I}
+ **/
+function unwrapSome(item) {
+    return item ?? (() => { throw new Error('unwrap on `null` value') })();
+}
+
+/** @returns {UserStatisticElems} */
+function getUserStatisticElems() {
+    const container = unwrapSome(/** @type {HTMLButtonElement} */(document.getElementById("user-statistic")));
+    if (container.nodeName !== "BUTTON") {
+        throw new Error('id="user-statistic element" found but is not a Button');
+    }
+    const year = unwrapSome(document.getElementById("statistic-year"));
+    const month = unwrapSome(document.getElementById("statistic-month"));
+    const week = unwrapSome(document.getElementById("statistic-week"));
+    const day = unwrapSome(document.getElementById("statistic-day"));
+    const hour = unwrapSome(document.getElementById("statistic-hour"));
+    const minute = unwrapSome(document.getElementById("statistic-minute"));
+    const click = document.getElementById("statistic-click");
+
+    return {
+        container,
+        year,
+        month,
+        week,
+        day,
+        hour,
+        minute,
+        click,
+    }
+}
+
+const MINUTES_PER_CLICK = 30;
+
+class UserStatistic {
+    /** @type {DisplayState} */
+    #state;
+    /** @type {number} */
+    #click_count;
+    /** @type {UserStatisticElems} */
+    #elems;
+
+    /** @param {UserStatisticElems} elems*/
+    constructor(elems) {
+        this.#state = new DisplayState(
+            Number(localStorage.getItem("user-statistic-state")) || UserStatisticState.AddedDuration,
+            Object.keys(UserStatisticState).length,
+            "user-statistic-state"
+        );
+        this.#click_count = Number(localStorage.getItem("battlebit-click-count")) || 0;
+        this.#elems = elems;
+    }
+
+    /**
+     * @param {number} minutes
+     * @returns {UnitDuration[]}
+     **/
+    #calculateDuration(minutes) {
+        const time_units_length = Object.keys(TimeUnits).length;
+        let duration = [];
+
+        let remaining_minutes = minutes;
+
+        for (let i = time_units_length - 1; i >= 0; i--) {
+            const minutes_per_unit = MINUTES_PER_UNIT[i];
+            if (remaining_minutes > minutes_per_unit) {
+                duration.push({ value: remaining_minutes / minutes_per_unit, time_unit: i });
+                remaining_minutes = remaining_minutes % minutes_per_unit;
+            }
+        }
+
+        return duration;
+    }
+
+    /**
+     * Although each click randomly adds 25-35 minutes to the datetime, it
+     * eventually converges to 30 minutes per click because we know it's
+     * uniformly distributed
+     *
+     * @returns {number}
+     **/
+    #getAddedMinutes() {
+        // Add 25-35 minutes at the end to fake some randomness
+        const random_minutes = (Math.random() * 10) + 25;
+        return ((this.#click_count - 1) * MINUTES_PER_CLICK) + random_minutes;
+    }
+
+    /**
+     * @param {string} id
+     * @returns {HTMLSpanElement}
+     **/
+    #createAndAppendStatisticElement(id) {
+        const elem = document.createElement("span");
+        elem.id = id;
+        this.#elems.container.appendChild(elem);
+        return elem;
+    }
+
+    #tryCreateDurationElems() {
+        this.#elems.year = this.#elems.year ?? this.#createAndAppendStatisticElement("statistic-year");
+        this.#elems.month = this.#elems.month ?? this.#createAndAppendStatisticElement("statistic-month");
+        this.#elems.week = this.#elems.week ?? this.#createAndAppendStatisticElement("statistic-week");
+        this.#elems.day = this.#elems.day ?? this.#createAndAppendStatisticElement("statistic-day");
+        this.#elems.hour = this.#elems.hour ?? this.#createAndAppendStatisticElement("statistic-hour");
+        this.#elems.minute = this.#elems.minute ?? this.#createAndAppendStatisticElement("statistic-minute");
+    }
+
+    #createDisplayElems() {
+        switch (this.#state.state) {
+            case UserStatisticState.AddedDuration: {
+                this.#elems.click?.remove();
+                this.#elems.click = null;
+                this.#tryCreateDurationElems();
+                break;
+            }
+            case UserStatisticState.ClickCount: {
+                const click_count_elem = document.createElement("span");
+                click_count_elem.id = "statistic-click";
+                click_count_elem.className = "dim-fg-color";
+                this.#elems.container.replaceChildren(click_count_elem);
+
+                this.#elems.year = null;
+                this.#elems.month = null;
+                this.#elems.week = null;
+                this.#elems.day = null;
+                this.#elems.hour = null;
+                this.#elems.minute = null;
+
+                this.#elems.click = click_count_elem;
+                break;
+            }
+            default:
+                throw new Error("Invalid state");
         }
     }
 
-    return `0${timeUnitToString(TimeUnits.Minute)}`;
+    cycleState() {
+        this.#state.cycleState();
+        this.#createDisplayElems();
+        this.#updateDisplayDOM();
+    }
+
+    incrementClickCount() {
+        this.#click_count++;
+        localStorage.setItem("battlebit-click-count", String(this.#click_count));
+        this.#updateDisplayDOM();
+    }
+
+    /**
+     * @param {number} time_unit
+     * @throws {Error}
+     * @returns {HTMLSpanElement}
+     **/
+    #getTimeUnitAsElem(time_unit) {
+        switch (time_unit) {
+            case TimeUnits.Year:
+                return unwrapSome(this.#elems.year);
+            case TimeUnits.Month:
+                return unwrapSome(this.#elems.month);
+            case TimeUnits.Week:
+                return unwrapSome(this.#elems.week);
+            case TimeUnits.Day:
+                return unwrapSome(this.#elems.day);
+            case TimeUnits.Hour:
+                return unwrapSome(this.#elems.hour);
+            case TimeUnits.Minute:
+                return unwrapSome(this.#elems.minute);
+            default:
+                throw new Error("invalid timeunit")
+        }
+    }
+
+    #updateDisplayDOM() {
+        switch (this.#state.state) {
+            case UserStatisticState.AddedDuration: {
+                const added_duration = this.#calculateDuration(this.#getAddedMinutes());
+                let greatest_present_unit = 0;
+                const absent_units = Array.from(Array(Object.keys(TimeUnits).length).keys());
+
+                for (let i = 0; i < added_duration.length; i++) {
+                    const unit_duration = added_duration[i];
+                    /** @type {HTMLSpanElement} */
+                    if (unit_duration.time_unit > greatest_present_unit) {
+                        greatest_present_unit = unit_duration.time_unit;
+                    }
+                    const elem = this.#getTimeUnitAsElem(unit_duration.time_unit);
+                    const text = formatUnitDuration(unit_duration.time_unit, Math.floor(unit_duration.value));
+                    if (elem.textContent !== text) {
+                        elem.textContent = text;
+                    }
+                    elem.style.display = "block";
+                    elem.className = "dim-fg-color";
+                    absent_units.splice(absent_units.indexOf(unit_duration.time_unit), 1);
+                }
+
+                // Set absent units to be a darker color instead of putting
+                // `display: none` so that they don't cause a DOM reflow & make it
+                // hard to watch the number. e.g. added duration of '3w 4d 20m' results in display of '3w 4d 00h 20m'
+                for (let i = 0; i < absent_units.length; i++) {
+                    const absent_time_unit = absent_units[i];
+                    const elem = this.#getTimeUnitAsElem(absent_time_unit);
+                    const text = formatUnitDuration(absent_time_unit, 0);
+                    if (elem.textContent !== text) {
+                        elem.textContent = text;
+                    }
+                    elem.style.display = "block";
+                    elem.className = "very-dim-fg-color ";
+                }
+
+                const time_units_length = Object.keys(TimeUnits).length;
+
+                // 'display: none' for all elements greater than the greatest
+                // present unit. e.g. greatest present unit is
+                // `TimeUnits.Week`, so year and month would be hidden.
+                for (let time_unit = Math.min(time_units_length - 1, greatest_present_unit + 1); time_unit < time_units_length ; time_unit++) {
+                    let elem = this.#getTimeUnitAsElem(time_unit);
+                    elem.style.display = "none";
+                    elem.textContent = "";
+                }
+                break;
+            }
+            case UserStatisticState.ClickCount: {
+                const click_count_elem = unwrapSome(this.#elems.click);
+                click_count_elem.textContent = String(this.#click_count);
+                break;
+            }
+
+            default:
+                throw new Error("Invalid state");
+        }
+    }
+
+    build() {
+        this.#createDisplayElems();
+        this.#updateDisplayDOM();
+        this.#elems.container.addEventListener("click", (_event) => {
+            this.cycleState();
+        });
+    }
 }
+
 
 class RefreshButton {
     /** @type {HTMLButtonElement} */
     #elem;
     /** @type {SVGElement} */
     #svg_elem;
-    /** @type {HTMLSpanElement} */
-    #click_count_elem;
-    /** @type {HTMLSpanElement} */
-    #added_duration_elem;
+
+    /** @type {UserStatistic} */
+    #user_statistic
 
     /** @type {Timeout} */
-    #reset_timeout;
+    #reset_rotation_timeout;
     /** @type {number} */
     #rotation;
-    /** @type {number} */
-    #num_clicks;
+    // /** @type {number} */
+    // #num_clicks;
 
     /**
      * @param {HTMLButtonElement} elem
      * @param {SVGElement} svg_elem
-     * @param {HTMLSpanElement} click_count_elem
-     * @param {HTMLSpanElement} added_duration_elem
      */
-    constructor(elem, svg_elem, click_count_elem, added_duration_elem) {
+    constructor(elem, svg_elem) {
         this.#elem = elem;
         this.#svg_elem = svg_elem;
-        this.#click_count_elem = click_count_elem;
-        this.#added_duration_elem = added_duration_elem;
+        this.#user_statistic = new UserStatistic(getUserStatisticElems());
         this.#rotation = 0;
-        this.#reset_timeout = new Timeout(this.#resetRotation.bind(this), REFRESH_BUTTON_TIMEOUT_DURATION);
-        this.#num_clicks = Number(localStorage.getItem("battlebit-clicks")) || 0;
-    }
-
-    #tryShowClickCount() {
-        if (this.#click_count_elem.classList.contains("hide")) {
-            if (this.#num_clicks >= 10) {
-                this.#click_count_elem.classList.remove("hide");
-            }
-        }
-    }
-
-    #displayAddedDuration() {
-        // Although each click randomly adds 25-35 minutes to the datetime,
-        // because it's uniformly distributed, it eventually converges
-        // to 30 minutes per click.
-        //
-        // Add 25-35 minutes to fake some randomness
-        const random_minutes = 25 + Math.random() * 10;
-        const added_duration_minutes = (this.#num_clicks - 1) * 30 + random_minutes;
-        this.#added_duration_elem.textContent = `${toShortDuration(added_duration_minutes)}`;
+        this.#reset_rotation_timeout = new Timeout(this.#resetRotation.bind(this), REFRESH_BUTTON_TIMEOUT_DURATION);
     }
 
     #onClick() {
@@ -1353,12 +1605,8 @@ class RefreshButton {
                 this.#elem.disabled = false;
             }
 
-            const num_clicks = String(++this.#num_clicks);
-            localStorage.setItem("battlebit-clicks", num_clicks);
-            this.#tryShowClickCount();
-            this.#click_count_elem.textContent = num_clicks;
+            this.#user_statistic.incrementClickCount();
 
-            this.#displayAddedDuration();
         } else if (websocket.state() === WebSocket.CLOSED) {
             websocket.tryConnect();
         }
@@ -1387,7 +1635,7 @@ class RefreshButton {
         }
         this.#svg_elem.animate(keyframe, keyframe_timing);
 
-        this.#reset_timeout.restart()
+        this.#reset_rotation_timeout.restart()
     }
 
     #resetRotation() {
@@ -1405,7 +1653,7 @@ class RefreshButton {
         }
         this.#svg_elem.animate(keyframe, keyframe_timing);
 
-        this.#reset_timeout.cancel();
+        this.#reset_rotation_timeout.cancel();
     }
 
     build() {
@@ -1425,8 +1673,7 @@ class RefreshButton {
             }
         })
 
-        this.#click_count_elem.textContent = String(this.#num_clicks);
-        this.#displayAddedDuration();
+        this.#user_statistic.build();
     }
 }
 
@@ -1586,20 +1833,12 @@ document.addEventListener("DOMContentLoaded", (_event) => {
     const refresh_button_elem = /** @type {HTMLButtonElement} */(document.getElementById("refresh")) ?? assertElementExists("refresh");
     const refresh_svg_elem = /** @type {SVGElement | null} */(document.querySelector("#refresh>svg"));
     if (refresh_svg_elem === null) {
-        throw new Error("No svg element inside id=\"refresh\"");
+        throw new Error("No svg inside id=\"refresh\"");
     }
 
-    const click_count_elem = document.getElementById("user-click-count-statistic");
-    if (click_count_elem === null) {
-        throw new Error("No element with id=\"user-click-count-statistic\"");
-    }
+    const user_statistic = new UserStatistic(getUserStatisticElems());
 
-    const added_duration_elem = document.getElementById("user-added-duration-statistic");
-    if (added_duration_elem === null) {
-        throw new Error("No element with id=\"user-added-duration-statistic\"");
-    }
-
-    const refresh_button = new RefreshButton(refresh_button_elem, refresh_svg_elem, click_count_elem, added_duration_elem);
+    const refresh_button = new RefreshButton(refresh_button_elem, refresh_svg_elem);
     refresh_button.build();
 
     const countdown_elem = document.getElementById("countdown");
