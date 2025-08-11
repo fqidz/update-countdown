aa41d8ebcbeef981a8078360db0e052567e08a3e
fqidz <faidz.arante@gmail.com>
2025-08-12 02:00:54 +0300

    fix: build.rs favicons

--- build.rs
+++ build.rs
@@ -1,14 +1,16 @@
 use std::{fs, path::Path, process::Command};
 
-const OUTPUT_PATH: &str = "./dist";
-const ESBUILD_PATHS: &[&str] = &["assets/scripts", "assets/css", "assets/favicons"];
+const ESBUILD_PATHS: &[&str] = &["assets/scripts", "assets/css"];
 const MINIFY_PATHS: &[&str] = &["templates"];
+const FAVICONS_PATH: &str = "assets/favicons";
 
 /// Build script that uses 'esbuild' and 'minify' to compile & minify js, css, html, etc. Chose
 /// 'esbuild' so that it can compile js modules into one file, and 'minify' to minify the html
 /// templates.
 fn main() {
+    let output_path = std::env::var("OUTPUT_PATH").unwrap_or_else(|_| "dist".to_string());
     println!("cargo::rerun-if-changed=build.rs");
+    println!("cargo::rerun-if-changed={}", output_path);
 
     let _ = vec!["assets", "templates"]
         .iter()
@@ -23,7 +25,7 @@ fn main() {
         panic!("\x1b[1;31m'minify' not installed (https://github.com/tdewolff/minify)\x1b[0m");
     }
 
-    let output_path = Path::new(OUTPUT_PATH);
+    let output_path = Path::new(&output_path);
     // Delete previous `OUTPUT_PATH` directory to avoid overwriting issues (probably).
     if output_path.is_dir() {
         fs::remove_dir_all(output_path).unwrap();
@@ -34,19 +36,25 @@ fn main() {
         .arg("--bundle")
         .arg("--minify")
         .arg("--target=es2020")
-        .arg("--loader:.png=file")
-        .arg("--loader:.ico=file")
-        .arg("--loader:.svg=text")
-        .arg("--loader:.webmanifest=text")
-        .arg(format!("--outdir={}/assets", OUTPUT_PATH))
-        .output()
+        .arg(format!("--outdir={}", &output_path.join("assets").to_str().unwrap()))
+        .status()
         .unwrap();
 
     Command::new("minify")
+        .arg("--sync")
         .arg("--recursive")
         .args(MINIFY_PATHS)
         .arg("--output")
         .arg(output_path.to_str().unwrap())
-        .output()
+        .status()
+        .unwrap();
+
+    // TODO: use walkdir
+    Command::new("cp")
+        .arg("-r")
+        .arg("-t")
+        .arg(output_path.join("assets").to_str().unwrap())
+        .arg(FAVICONS_PATH)
+        .status()
         .unwrap();
 }
